a9f8e092a407345d63eb45fca03c10349e4df733
Ryan Lee <rjlguitar@gmail.com>
2021-02-10 10:22:18 -0800

    fixed navbar and styles

--- spd-project/src/Navbar.js
+++ spd-project/src/Navbar.js
@@ -1,4 +1,5 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
+import './Navbar.css';
 
 function Navbar() {
   return (
@@ -23,6 +24,51 @@ function Navbar() {
             </a>
           </div>
         </div>
+      </div>
+      <div class='pull-right ml-auto'>
+        <ul class='nav navbar-nav navbar-right'>
+          <li class='nav-item dropdown'>
+            <a
+              class='nav-link dropdown-toggle'
+              href='http://example.com'
+              id='navbarDropdownMenuLink'
+              data-toggle='dropdown'
+              aria-haspopup='true'
+              aria-expanded='false'>
+              Login
+            </a>
+            <div class='dropdown-menu'>
+              <form
+                class='form-horizontal'
+                method='post'
+                accept-charset='UTF-8'>
+                <input
+                  class='form-control login'
+                  type='text'
+                  name='username'
+                  placeholder='Username..'
+                />
+                <br />
+                <input
+                  class='form-control login'
+                  type='password'
+                  name='password'
+                  placeholder='Password..'
+                />
+                <br />
+                <button class='btn btn-dark'>Login</button>
+              </form>
+            </div>
+          </li>
+          <li class='nav-item navbar-right'>
+            <a
+              className='nav-link'
+              activeClassName='nav-link-active'
+              to='/signup'>
+              Sign Up
+            </a>
+          </li>
+        </ul>
       </div>
     </nav>
   );
