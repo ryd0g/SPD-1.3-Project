0f7038eb5e12929c3aa24faa86ca005671c65f9b
Ryan Lee <rjlguitar@gmail.com>
2021-02-10 10:22:27 -0800

    imported jquery

--- spd-project/src/index.js
+++ spd-project/src/index.js
@@ -1,3 +1,7 @@
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/js/bootstrap.js';
+import $ from 'jquery';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
